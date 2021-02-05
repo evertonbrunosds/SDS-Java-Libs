583f6fcb925774721261d15caddd12f92356296b
evertonbrunosds <evertonbrunosds@gmail.com>
2021-02-05 12:42:57 -0300

    SDSUL (v1.0)

---
+++ src/evertonbrunosds/SDSUtilityLib/v1/util/AccountingExpenseRequisition.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the SDSUtilityLib Open Source Project.
+ * SDSUtilityLib is licensed under the GNU GPLv3.
+ *
+ * Copyright © 2020. Everton Bruno Silva dos Santos <evertonbrunogithub@yahoo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package evertonbrunosds.SDSUtilityLib.v1.util;
+
+/**
+ * Classe responsável por comportar-se como requisição de despesa contábil.
+ * @author Everton Bruno Silva dos Santos.
+ * @version 1.0
+ * @since 1.0
+ */
+public class AccountingExpenseRequisition {
+    
+}
